77d03c1f3267d3325636300c3703ab189d533d5b
Mohamed Elhossin <32275322+MohamedElhossin@users.noreply.github.com>
2020-07-05 02:33:55 +0200

    Update webService.py

--- webService.py
+++ webService.py
@@ -3,28 +3,24 @@ import numpy as np
 import base64
 import io
 from PIL import Image
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
+from google.cloud import storage
+from keras.models import load_model
+from keras.preprocessing import image
 import logging
 import os
 import h5py
-app = Flask(__name__)
 
-import tensorflow as tf 
-import PIL
-import flask
-print(.__version__)
+app = Flask(__name__)
+CLOUD_STORAGE_BUCKET = os.environ['CLOUD_STORAGE_BUCKET']
 
 def loadmodel():
-  """
-  # this section to deploy web service on google app engine 
-
   storage_client = storage.Client()
   bucket = storage_client.get_bucket(CLOUD_STORAGE_BUCKET)
   blob = bucket.blob('classes_model_finalone.h5')
   blob.download_to_filename('./classes_model_finalone.h5')
-  """
-  model = load_model('/content/gdrive/My Drive/gd/mymodel.h5')
+  
+  model = load_model('./classes_model_finalone.h5')
+ 
   return model
 
 def decode_image(image_base64):
@@ -33,30 +29,37 @@ def decode_image(image_base64):
   final_image = image.img_to_array(image_convert)
   final_image = Image.Image.resize(image_convert, (200, 200))
   predict_image = np.expand_dims(final_image, axis=0)
+  
   return predict_image
 
 def get_monument(data):
-    prop = np.max(data)
-    index = np.argmax(data)
-    if index == 0:
-       monument = 'Great Pyramid of Giza'
-
-    elif index == 1:
-       monument = 'Great Sphinx of Giza'
-
-    return monument
+  prop = np.max(data)
+  index = np.argmax(data)
+  
+  if index == 0:
+     monument = 'Great Pyramid of Giza'
+  elif index == 1:
+     monument = 'Abu Simbel temples'
+  elif index == 2:
+     monument = 'Great Sphinx of Giza'
+  elif index == 3:
+     monument = 'Mortuary Temple of Hatshepsut'
+      
+  return monument
+  
 
 @app.route('/', methods=['POST'])
 def getPredection():
-    if request.headers['Content-Type'] == 'application/json':
-       data = request.json
-       newdata = data['Name']
+  if request.headers['Content-Type'] == 'application/json':       
+     data = request.json
+     newdata = data['Name']
 
-       image = decode_image(newdata)
-       model = loadmodel()
-       data = model.predict(image)
-       monument = get_monument(data)
-    return monument
+     image = decode_image(newdata)
+     model = loadmodel()
+     data = model.predict(image)
+     monument = get_monument(data)
+    
+     return monument
     
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8080, debug=True)
+  app.run(host='127.0.0.1', port=8080, debug=True)
